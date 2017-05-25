fc7c3f09a73b0eb0d98a4715aaa8b600245bc8fc
Dário Tavares Antunes <tavaresd@tcd.ie>
2017-05-25 01:44:20 +0100

    Add named status codes for convenience and readability

---
+++ code/src/main/java/util/StatusCodes.java
@@ -0,0 +1,15 @@
+package util;
+
+public class StatusCodes {
+
+	public static class ClientError {
+
+		public static final int UNAUTHORIZED = 401;
+	}
+
+	public static class Success {
+
+		public static final int CREATED = 201, ACCEPTED = 202, NO_CONTENT = 204;
+	}
+
+}
